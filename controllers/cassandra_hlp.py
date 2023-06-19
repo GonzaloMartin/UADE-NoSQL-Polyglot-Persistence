0061af3a29b30b93f79ec6f669b926ebddcb8a8a
federey99 <federicogabrielrey@gmail.com>
2023-06-19 18:08:28 -0300

    creacion de controladores
    se logra la conexión a las 3 bases de datos y se crean los helpers de cada base.

---
+++ controllers/cassandra_hlp.py
@@ -0,0 +1,43 @@
+#cqlsh para arrancar cassandra
+from cassandra.cluster import Cluster
+
+#en cassandra keyspace = BDD
+
+class CassandraHelper:
+    def __init__(self):
+        self._cluster = None
+        self._session = None
+        self._keyspace = None
+
+    def conectar(self):
+        self._cluster = Cluster(['127.0.0.1','7000'])
+        self._session = self._cluster.connect()
+
+    def usar_db(self, keyspace):
+        self._session.set_keyspace(keyspace)
+
+    #obtener bdd actual
+    def getKeyspace(self):
+        return self._session.keyspace()
+
+    def execute_query(self, query):
+        return self._session.execute(query)
+
+    def get_all_documents(self, table):
+        query = f"SELECT * FROM {table}"
+        result = self.execute_query(query)
+        return list(result)
+
+    def insert_document(self, table, columns, values):
+        query = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join(values)})"
+        self.execute_query(query)
+
+    def update_document(self, table, set_values, where_clause):
+        query = f"UPDATE {table} SET {set_values} WHERE {where_clause}"
+        self.execute_query(query)
+
+    def close_connection(self):
+        self._session.shutdown()
+        self._cluster.shutdown()
+
+
---
+++ controllers/mongo_hlp.py
@@ -0,0 +1,99 @@
+#Para levantar mongo desde docker tipear mongosh
+from pymongo import MongoClient
+
+class MongoHelper:
+    def __init__(self):
+        self._client = None
+        self._db = None
+
+    def conectar(self, port='mongodb://127.0.0.1:27017/'):
+        self._client = MongoClient(port)
+
+    def usar_db(self, db_name):
+        self._db = self._client[db_name]
+
+    def get_collection(self, collection_name):
+        return self._db[collection_name]
+
+    def get_documents(self,collection):
+        coleccion = self._db[collection]
+        return list(coleccion.find())
+
+    def insert_document(self,collection,document):
+        coleccion = self._db[collection]
+        coleccion.insert_one(document)
+
+    #def update_document(self, filter, update):
+        #self._collection.update_one(filter, update)
+
+    def close_connection(self):
+        if self._client:
+            self._client.close()
+
+    def get_client(self):
+        return self._client
+
+
+
+
+
+
+"""
+cliente=MongoClient
+#para obtener una colección
+def conectarMongo():
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017/')
+        return client
+    except Exception as e:
+        print(e)
+
+def setClient(client):
+    cliente=client
+
+def getClient():
+    return cliente
+
+def getColeccion(client,base,coleccion):
+    try:
+        conectarMongo()
+        db = client[base]#'bdd2'
+        # Accede a una colección en la base de datos
+        collection = db[coleccion] #'usuarios'
+        return collection
+    except Exception as e:
+        print(e)
+
+def setDocumento(documento,collection):
+    try:
+        result = collection.insert_one(documento)
+        return result.inserted_id
+    except Exception as e:
+        print("Error al insertar documento. Error: "+e)
+    
+def getDocumento(client,base,collection):
+    getColeccion(client,base,collection)
+    documents = collection.find()
+
+
+
+nuevo_documento = {
+    "nombre": "Lenovo",
+    "apellido": "ThinkPad"
+}
+
+# Inserta el nuevo documento en la colección
+result = collection.insert_one(nuevo_documento)
+
+# Imprime el ID del documento recién insertado
+print("Documento insertado. ID:", result.inserted_id)
+
+# Realiza operaciones en la colección, por ejemplo, obtén los documentos
+documents = collection.find()
+
+documents = collection.find({}, {"nombre": 1})
+
+# Itera sobre los documentos e imprime la marca
+for document in documents:
+    print(document["nombre"])"""
+
