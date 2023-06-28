7d8e106aa2912538338d1c99c983b65d8de3c9ce
Gonzalo Martin <montalvog@gmail.com>
2023-06-28 19:14:39 -0300

    ConfirmarCarrito Stuff

--- models/views.py
+++ models/views.py
@@ -1,9 +1,11 @@
 from controllers.LoginController import *
 from controllers.buscarProducto import BuscarProducto
+from controllers.confirmar_carrito import ConfirmarCarrito
 
 class Views:
     def __init__(self):
         self.buscar_producto = BuscarProducto()
+        self.confirmar_carrito = ConfirmarCarrito()
 
     def display_login_screen(self):
         print("=== Login ===")
@@ -88,3 +90,48 @@ class Views:
         opcion = int(input("opcion: "))
         return opcion
 
+    def display_confirmar_carrito_view(self, carrito, cliente):
+        print("-- Confirmación de Carrito --")
+        print("ID Producto - Descripción - Cantidad - Precio Unitario")  # header
+        for item in carrito:
+            print(f"{item['id']} - {item['descripcion']} - {item['cantidad']} - {item['precio_unitario']}")
+
+        impuestos = self.confirmar_carrito.calcular_impuestos(carrito)
+        print(f"Impuestos IVA 21%: {impuestos}")
+
+        tipo_descuento, descuento_porcentaje, importe_descuento = self.confirmar_carrito.calcular_descuento(cliente)
+        print(f"Descuentos: {tipo_descuento} ({descuento_porcentaje}%) -{importe_descuento}")
+
+        importe_total = self.confirmar_carrito.calcular_importe_total(carrito, impuestos, importe_descuento)
+        print(f"Importe TOTAL: {importe_total}")
+
+        print("\nDatos del cliente:")
+        print(f"Nombre y Apellido: {cliente['nombre']} {cliente['apellido']}")
+        print(f"Dirección: {cliente['direccion']}")
+        print(f"DNI: {cliente['dni']}")
+
+        print("\n1. Confirmar")
+        print("2. Volver")
+
+        opcion = int(input("Seleccionar: "))
+        if opcion == 1:
+            self.procesar_confirmacion_carrito(carrito)
+        elif opcion == 2:
+            return
+        else:
+            print("Opción inválida.")
+
+    def procesar_confirmacion_carrito(self, carrito):
+        print("Elija un Tipo de pago para Finalizar la compra:\n"
+              "1. Efectivo.\n"
+              "2. Tarjeta de Crédito.\n"
+              "3. Cuenta Corriente.\n"
+              "0. Volver.\n")
+        opcion = input("Seleccionar tipo de pago: ")
+        if opcion == 1 or opcion == 2 or opcion == 3:
+            print("Compra confirmada.")
+            self.confirmar_carrito.insertar_datos_cassandra(carrito)
+        elif opcion == 0:
+            return
+        else:
+            print("Opción inválida.")
